0c2d89415d1e299b7a27d5ebbb84cec4c46c947e
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2025-05-23 17:55:13 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,9 @@ import time
 
 @st.cache_data
 def longproc():
+  st.util.write( 'Loop micmicking data generation / long process' )
   y=0
-  for x in range(5):
+  for x in range(20):
     time.sleep(1)
     y = y + 1
   return y
@@ -16,6 +17,7 @@ if 'key' not in st.session_state:
     if(st.button('Set Key')):
       y=longproc()
   st.session_state['key'] = 'value'
+  utils.st.write( f'Generated data: {y}')
 
 if 'key' in st.session_state:
   peace.empty()
@@ -24,4 +26,7 @@ st.write(st.session_state.key)
 
 if(st.button('Set New Session')):
   st.write('new')
-  
+  try:
+    utils.st.write( f'Generated data: {y}')
+  except:
+    utils.st.write( f'data cleared from cache')
